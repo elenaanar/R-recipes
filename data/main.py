2491810abe6d3d74023c4b6ec77b9d03a03d62a9
k-main <kennkmaina@mgail.com>
2024-01-20 14:08:06 -0800

    fiaojsef

--- data/main.py
+++ data/main.py
@@ -3,13 +3,13 @@ import subprocess
 
 def get_items(list):
     item_count = 0
-    subprocess.run("wget -")
+    for item in list:
+        subprocess.run("wget -O list_page_files/recipe_list_%d.html %s" % (item_count, list[item_count]), shell=True)
+        item_count += 1
 
 with open("list_page.html", 'r', encoding='UTF-8') as inputFile:
     page_content = inputFile.read()
     soup = bs(page_content, 'lxml')
     item_wrappers = soup.find_all('a', class_='link-list__link type--dog-bold type--dog-link')
     item_links = [(str(item).split()[4][6:-1]) for item in item_wrappers]
-    short = item_links[0:5]
-    
-    subprocess.run("pwd", shell=True)
+    get_items(item_links)
