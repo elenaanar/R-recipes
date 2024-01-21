0b95627a86db567d274922e056355f959f364fc8
Elena <elenamardi03@gmail.com>
2024-01-21 00:37:06 -0800

    fixed import for card

--- r-recipes/src/App.jsx
+++ r-recipes/src/App.jsx
@@ -1,7 +1,9 @@
-import {useState} from 'react';
-import './App.css'
+// App.jsx
+import React from 'react';
+import './App.css';
 import RecipeCard from './Components/RecipeCard';
-import { SearchBar } from './Components/SearchBar';
+import {SearchBar} from './Components/SearchBar';
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +11,16 @@ function App() {
       <div className="search-bar-container">
         <div className="top-bar"></div>
         <SearchBar />
-        <div>SearchResults</div>
       </div>
-      <RecipeCard
-        title="Delicious Pasta"
-        description="A mouth-watering pasta recipe with a rich tomato sauce."
-        image="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1615916524567.jpeg"
-        recipeLink="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689"
-      />
+      <div className="recipe-cards-container">
+        <RecipeCard
+          title="Delicious Pasta"
+          description="A mouth-watering pasta recipe with a rich tomato sauce."
+          image="https://example.com/pasta-thumbnail.jpg"
+          recipeLink="https://example.com/pasta-recipe"
+        />
+        {/* Add more RecipeCard components here as needed */}
+      </div>
     </div>
   );
 }
--- r-recipes/src/Components/RecipeCard.jsx
+++ r-recipes/src/Components/RecipeCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './RecipeCard.css';
 
 
 const RecipeCard = ({ title, description, image, recipeLink }) => {
