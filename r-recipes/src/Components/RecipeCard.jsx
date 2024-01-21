8a35fa773b7959a45086786718dfd824570e735a
Elena <elenamardi03@gmail.com>
2024-01-20 23:01:25 -0800

    progress on recipe cards

---
+++ r-recipes/src/Components/RecipeCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const RecipeCard = ({ title, description, image, recipeLink }) => {
+  return (
+    <div className="recipe-card">
+      <img src={image} alt={title} className="recipe-image" style={{ maxWidth: '100%', maxHeight: '200px' }} />
+      <div className="recipe-details">
+        <h2 className="recipe-title">{title}</h2>
+        <p className="recipe-description">{description}</p>
+        <a href={recipeLink} target="_blank" rel="noopener noreferrer">
+          <button className="recipe-button">View Recipe</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+RecipeCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  recipeLink: PropTypes.string.isRequired,
+};
+
+export default RecipeCard;
+
