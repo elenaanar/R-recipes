476d9e61a1696e92e3f86728a39ebb942fcc50fd
arrdyas <arrdyas@gmail.com>
2024-01-21 06:14:37 -0800

    Fixed grid

--- r-recipes/src/App.jsx
+++ r-recipes/src/App.jsx
@@ -3,25 +3,51 @@ import "./App.css";
 import RecipeCard from "./Components/RecipeCard";
 import { SearchBar } from "./Components/SearchBar";
 function App() {
-  const [data, setData] = useState([])
-  const [ingredients, setIngredients] = useState(['milk', 'sugar', 'flour', 'salt', 'chicken', 'apple', 'banana'])
+  const [data, setData] = useState([]);
+  const [ingredients, setIngredients] = useState([
+    "milk",
+    "sugar",
+    "flour",
+    "salt",
+    "chicken",
+    "apple",
+    "banana",
+  ]);
   return (
     <div className="App">
       <div className="top-bar"></div>
       <div className="main-page">
-        <div className="title-textbox search-bar-container">
-          R'recipes
-          <SearchBar data={data} setData={setData} ingredients={ingredients} setIngredients={setIngredients}/>
+        <div className="search-bar-container">
+          <div className="title-textbox">R'recipes</div>
+          <SearchBar
+            data={data}
+            setData={setData}
+            ingredients={ingredients}
+            setIngredients={setIngredients}
+          />
           {console.log(data)}
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              alignItems: "center",
+              justifyContent: "center",
+              maxWidth: "1085px",
+            }}
+          >
+            {data.map((r) => {
+              return (
+                <RecipeCard
+                  key={r._id}
+                  title={r.title}
+                  description={r.description}
+                  image={r.img}
+                  recipeLink={r.url}
+                ></RecipeCard>
+              );
+            })}
           </div>
-          {
-            data.map((r) => { return <RecipeCard 
-              key={r._id}
-              title={r.title} 
-              description={r.description} 
-              image={r.img} 
-              recipeLink={r.url}></RecipeCard>})
-          }
+        </div>
       </div>
     </div>
   );
