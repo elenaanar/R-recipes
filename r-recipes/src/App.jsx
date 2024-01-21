2af4089f2f25e1ab2948ab8d3428c550f78df1c2
Elena <elenamardi03@gmail.com>
2024-01-20 22:10:53 -0800

    search bar complete

--- r-recipes/src/App.jsx
+++ r-recipes/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState} from 'react';
+import {useState} from 'react';
 import './App.css'
 import { SearchBar } from './Components/SearchBar';
 function App() {
   return (
     <div className="App">
       <div className="search-bar-container">
-        <SearchBar />
-        <div>SearchResults</div>
+      <div className="top-bar"></div>
+      <div className="title-textbox">R'recipes</div>
+      <SearchBar />
+      {/* <div>SearchResults</div> */}
       </div>
     </div>
   );
