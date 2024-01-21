664fd94589635e41c59ed4190b4e876a17725a0a
AnthonyPadilla <hakarikinji@LAPTOP-N1O841M1.home>
2024-01-21 06:47:33 -0800

    I wanna shoot myself

--- r-recipes/src/Components/Sidebar.jsx
+++ r-recipes/src/Components/Sidebar.jsx
@@ -1,21 +1,32 @@
-// Sidebar.js
 import React, { useState } from 'react';
 import ItemList from './ItemList';
 import InputComponent from './InputComponent';
-import "../style.css"
+import "../style.css";
 
 const Sidebar = () => {
   const [items, setItems] = useState([]);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   const handleAddItem = (item) => {
     // Update the items state with the new item
     setItems([...items, item]);
   };
 
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="sidebar-container">
-      <ItemList items={items} />
-      <InputComponent onAddItem={handleAddItem} />
+    <div className={`sidebar-container ${isCollapsed ? 'collapsed' : ''}`}>
+      <div className="toggle-button" onClick={toggleCollapse}>
+        {isCollapsed ? '»' : '«'}
+      </div>
+      {!isCollapsed && (
+        <>
+          <ItemList items={items} />
+          <InputComponent onAddItem={handleAddItem} />
+        </>
+      )}
     </div>
   );
 };
